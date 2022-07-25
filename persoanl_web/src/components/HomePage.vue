17ee1b57f39572b052a041bdc5f5ce42da305698
arthur262 <46006708+arthur262@users.noreply.github.com>
2022-07-25 12:16:33 -0300

    initalized

---
+++ persoanl_web/src/components/HomePage.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container"></div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "HomePage",
+  data() {
+    return {
+      url: "",
+    };
+  },
+});
+</script>
+
+<style scoped lang="less">
+.container {
+  width: 100%;
+  height: fit-content;
+  background-image: url("http://arthur1.oss-us-west-1.aliyuncs.com/self-web/image/%E5%9B%BE%E7%89%87.png");
+  background-size: 100% 100%;
+}
+</style>
---
+++ persoanl_web/src/router/index.ts
@@ -0,0 +1,21 @@
+import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: "/",
+    name: "home",
+    redirect: "./Home",
+  },
+  {
+    path: "/Home",
+    name: "Home",
+    component: () => import("@/components/HomePage.vue"),
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+});
+
+export default router;
