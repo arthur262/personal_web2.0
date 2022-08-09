8ccbadb0b3680ddae9c773d10ba4742a5fc9d06a
arthur262 <46006708+arthur262@users.noreply.github.com>
2022-08-08 22:46:59 -0300

    2

--- vitePersonalweb/src/components/CV/CV_Project.vue
+++ vitePersonalweb/src/components/CV/CV_Project.vue
@@ -1,14 +1,10 @@
 <template>
-  <div class="box" id="Project">
-    <div style="display: flex">
-      <!--<bars-outlined/>-->
-      <h2 class="w2">{{ datas.Projects }}</h2>
-    </div>
+  <div >
     <div class="card">
       <div
         v-for="(iteam, index) in datas.Project"
         :key="index"
-        style="height: fit-content; width: 40ch; margin: 0 auto"
+        
       >
         <a-card hoverable class="container" @click="move">
           <div style="margin: 0 auto; width: fit-content; margin-bottom: 2ch">
@@ -34,10 +30,10 @@
               <div v-show="describe_OP">
                 <ol
                   class="C_content"
-                  v-for="iteamson in iteam.children"
-                  :key="iteamson.id"
+                  v-for=" ({ iteamson }, index) in iteam.children"
+                  :key="index"
                 >
-                  <li>{{ iteamson.details }}</li>
+                  <li>{{ iteamson }}</li>
                 </ol>
               </div>
               <fullscreen-outlined class="icon" />
@@ -78,9 +74,7 @@ export default {
 <style scoped>
 
 .card {
-  display: flex;
-  width: 100%;
-  margin: 0 auto;
+  width:inheritence;
   height: fit-content;
   border-radius: 1.5ch;
 }
