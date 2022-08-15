2bb3ab5a1246ea82f7fe283f4c079a852379540f
arthur262 <46006708+arthur262@users.noreply.github.com>
2022-08-14 21:33:40 -0300

    2

--- vitePersonalweb/src/views/PC/Photo.vue
+++ vitePersonalweb/src/views/PC/Photo.vue
@@ -1,35 +1,92 @@
 <template>
-  <div style="width:100%;background-color:whitesmoke;min-height:100vh;">
-    <a-layout class="Photo">
-      <a-page-header style="border: 0px solid rgb(235, 237, 240); margin-left: 2%" title="Photo"
-        @back="() => $router.back()" />
-      <div>
-      </div>
-      <a-back-top />
-    </a-layout>
+  <div style="width: 100%; min-height: 100vh">
+    <header class="backpic">
+    
+      <ul class="menu">
+        
+        <li>
+          <router-link
+            to="/Programmer"
+            class="link middle"
+            >Codder
+          </router-link>
+        </li>
+        <li>
+          <router-link
+            to="/Home"
+            class="link middle"
+
+          >
+            Home</router-link
+          >
+        </li>
+        <li>
+          <router-link
+            to="/Student"
+            class="link middle"
+          >
+            Student</router-link
+          >
+        </li>
+      </ul>
+      <!-- <div /> -->
+    </header>
+    <section></section>
+    <a-back-top />
   </div>
 </template>
 
 <script lang="ts">
-
-export default ({
-  components: {  },
+export default {
+  components: {},
   setup() {
-    return {
-    };
+    return {};
   },
   data() {
-    return {
-    };
+    return {};
   },
   mounted: function () {},
   methods: {},
-});
+};
 </script>
 
 <style scoped>
-.Photo {
-  max-width: 140ch;
+.menu {
+  list-style: none;
+  display: flex;
+  width: fit-content;
   margin: 0 auto;
+  padding: 1vh 0 0 0;
+  z-index: 10;
+}
+
+.link {
+  
+  color: transparent;
+  
+-webkit-text-stroke:2px rgba(245, 245, 245, 0.7);
+  font-size: 2.5em;
+  font-family: "Helvetica";
+  font-weight:bolder;  
+}
+.menu :hover a {
+  color: #8eb8de;
+  -webkit-text-stroke:0 rgba(245, 245, 245, 0.7);
+  
+}
+
+.middle {
+  margin: 0 1vw;
+  text-transform: uppercase;
+  font-family: "Executive-Medium";
+}
+.backpic{
+  background-image: url("/Photo/A7M2 (208).jpg");
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+  width:100vw;
+  height: 100vh;
+  z-index: 1;
 }
 </style>
