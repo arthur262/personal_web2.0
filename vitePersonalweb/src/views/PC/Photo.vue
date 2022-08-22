75cb71437f706584918db24ad8e9875c370bd0bf
arthur262 <46006708+arthur262@users.noreply.github.com>
2022-08-21 21:29:23 -0300

    10

--- vitePersonalweb/src/views/PC/Photo.vue
+++ vitePersonalweb/src/views/PC/Photo.vue
@@ -1,76 +1,69 @@
 <script setup lang="ts">
-import { DoubleRightOutlined,ArrowRightOutlined } from "@ant-design/icons-vue";
+import { DoubleRightOutlined, ArrowRightOutlined } from "@ant-design/icons-vue";
 </script>
 <template>
   <div style="width: 100%; min-height: 100vh">
+  <audio src="http://arthur2.oss-cn-beijing.aliyuncs.com/Photopage/Y2Mate.is%20-%20%E8%BF%99%E4%B8%96%E7%95%8C%E9%82%A3%E4%B9%88%E5%A4%9A%E4%BA%BA%20-%20%E8%8E%AB%E6%96%87%E8%94%9A%20%E7%94%B5%E5%BD%B1%E3%80%8A%E6%88%91%E8%A6%81%E6%88%91%E4%BB%AC%E5%9C%A8%E4%B8%80%E8%B5%B7%E3%80%8B%E4%B8%BB%E9%A2%98%E6%9B%B2%E3%80%90%E5%8A%A8%E6%80%81%E6%AD%8C%E8%AF%8DMusic%20Lyrics%E3%80%91-i5L_etk6Tzo-160k-1660290728197.aac">
+  </audio>
+  
     <section>
-
-      <a-carousel  autoplay :dots="dos" style="z-index: 1;">
-        <div
-          v-for="iteam in DataBase"
-          :key="iteam"
-          class="background-image"
-        >
-        
-        <img :src="iteam" style="height:100vh;width:100vw;position:absolute;">
-          
+      <a-carousel autoplay :dots="dos" style="z-index: 1">
+        <div v-for="iteam in DataBase" :key="iteam" class="background-image">
+          <img
+            :src="iteam"
+            style="height: 100vh; width: 100vw; position: absolute"
+          />
         </div>
       </a-carousel>
 
-      <header style="position:absolute;width:100%;height:100vh;top:0;">
-            <a-row type="flex" style="padding: 4vh 8vw 0 8vw">
-              <a-col :flex="2">
-                <router-link to="/Photograph">
-                  <img
-                    src="/Photo/Artsy.png"
-                    style="opacity: 0.7"
-                    height="120"
-                  />
+      <header style="position: absolute; width: 100%; height: 100vh; top: 0">
+        <a-row type="flex" style="padding: 4vh 8vw 0 8vw">
+          <a-col :flex="2">
+            <router-link to="/Photograph">
+              <img src="http://arthur2.oss-cn-beijing.aliyuncs.com/Photopage/Artsy.png" style="opacity: 0.7" height="120" />
+            </router-link>
+          </a-col>
+          <a-col :flex="2">
+            <ul id="menu" ref="menu" class="animation-container">
+              <li>
+                <router-link to="/Home" class="link"> Home</router-link>
+              </li>
+              <li>
+                <router-link to="/Programmer" class="link"
+                  >Programmer
                 </router-link>
-              </a-col>
-              <a-col :flex="2">
-                <ul id="menu" ref="menu" class="animation-container">
-                  <li>
-                    <router-link to="/Home" class="link"> Home</router-link>
-                  </li>
-                  <li>
-                    <router-link to="/Programmer" class="link"
-                      >Programmer
-                    </router-link>
-                  </li>
-                  <li>
-                    <router-link to="/Student" class="link">
-                      Student</router-link
-                    >
-                  </li>
-                </ul>
-              </a-col>
-            </a-row>
-            <div class="themeword">
-        <h2
-          style="
-            font-family: 'Times New Roman', Times, serif;
-            letter-spacing: 0.333333em;
-            font-size: 2.5em;
-            white-space: nowrap;
-            font-style: italic;
-            margin: 0;
-          "
-          class="animation-container"
-          ref="travel"
-        >
-          Travel
-        </h2>
-        <div style="margin: 0 auto; width: fit-content">
-          <double-right-outlined
-            :style="{ fontSize: '32px'}"
-            class="roundloop"
-          />
+              </li>
+              <li>
+                <router-link to="/Student" class="link"> Student</router-link>
+              </li>
+            </ul>
+          </a-col>
+        </a-row>
+        <div class="themeword">
+          <h2
+            style="
+              font-family: 'Times New Roman', Times, serif;
+              letter-spacing: 0.333333em;
+              font-size: 2.5em;
+              white-space: nowrap;
+              font-style: italic;
+              margin: 0;
+            "
+            class="animation-container"
+            ref="travel"
+          >
+            Travel
+          </h2>
+          <div style="margin: 0 auto; width: fit-content">
+            <double-right-outlined
+              :style="{ fontSize: '32px' }"
+              class="roundloop"
+            />
+          </div>
         </div>
-      </div>
-          </header>
+      </header>
     </section>
-
+    
     <section>
       <div style="max-width: 140ch; margin: 0 auto">
         <section>
@@ -93,15 +86,13 @@ import { DoubleRightOutlined,ArrowRightOutlined } from "@ant-design/icons-vue";
       </div>
     </section>
 
-   <Photo_random :datas="ForRandom"/>
+    <Photo_random :datas="ForRandom" />
 
     <section>
       <div
         style="min-height: 110vh; background-color: black; position: relative"
       >
-        <div
-          class="emotionimage"
-        >
+        <div class="emotionimage">
           <div style="height: fit-content; width: 80vw">
             <span style="float: right">
               <h1
@@ -122,7 +113,6 @@ import { DoubleRightOutlined,ArrowRightOutlined } from "@ant-design/icons-vue";
               <h4>
                 <router-link to="/" class="e-profile"> Profile </router-link>
               </h4>
-              
             </span>
           </div>
         </div>
@@ -139,23 +129,26 @@ import { DoubleRightOutlined,ArrowRightOutlined } from "@ant-design/icons-vue";
         muted
       ></video>
     </section>
+    
     <a-back-top />
   </div>
 </template>
 
 <script lang="ts">
+import { ref } from 'vue';
 import axios from "axios";
 import Photo_random from "../../components/Photo/Photo_random.vue";
 export default {
-  components: { DoubleRightOutlined,ArrowRightOutlined,Photo_random },
+  components: { DoubleRightOutlined, ArrowRightOutlined, Photo_random },
   setup() {
-    return {};
+    const bottom = ref<number>(10);
+    return {bottom};
   },
   data() {
     return {
       DataBase: [],
-      ForRandom:[],
-      dos:false,
+      ForRandom: [],
+      dos: false,
     };
   },
   mounted: function () {
@@ -184,38 +177,38 @@ export default {
 </script>
 
 <style scoped>
-.emotionimage{
-  background: url('/Photo/DSC00528-5.jpg') no-repeat;
-            width: 100%;
-            height: 110vh;
-            background-position: center;
-            background-size: cover;
-            position: absolute;
-            background-attachment: fixed;
-            z-index: 1;
-            display: flex;
-            justify-content: center;
-            align-items: center;
+.emotionimage {
+  background: url("http://arthur2.oss-cn-beijing.aliyuncs.com/Photopage/DSC00528-5.jpg")
+    no-repeat;
+  width: 100%;
+  height: 110vh;
+  background-position: center;
+  background-size: cover;
+  position: absolute;
+  background-attachment: fixed;
+  z-index: 1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
-.roundloop{
-  animation:loopanimation;
-  animation-timing-function: ease-in-out; 
-		animation-delay: 0s; 
-		animation-iteration-count: infinite; 
-		animation-duration: 1s; 
-    transform: rotate(90deg); 
+.roundloop {
+  animation: loopanimation;
+  animation-timing-function: ease-in-out;
+  animation-delay: 0s;
+  animation-iteration-count: infinite;
+  animation-duration: 1s;
+  transform: rotate(90deg);
 }
-@keyframes loopanimation{
-  0%{
-    transform: rotate(90deg) translateX(-5px); 
+@keyframes loopanimation {
+  0% {
+    transform: rotate(90deg) translateX(-5px);
   }
-  50%{
+  50% {
     transform: rotate(90deg) translateX(10px);
   }
-  100%{
-    transform:rotate(90deg) translateX(-5px);
+  100% {
+    transform: rotate(90deg) translateX(-5px);
   }
-
 }
 .e-profile {
   position: relative;
@@ -226,21 +219,20 @@ export default {
   letter-spacing: 0.133333em;
 }
 .e-profile:before {
-  position:absolute;
+  position: absolute;
   width: 0;
   left: 0;
   bottom: -0.1rem;
-  content: '';
-  transition: width .5s ease-in-out;
-  -webkit-transition: width .5s ease-in-out;
-   border-bottom:2px solid #8eb8de;
+  content: "";
+  transition: width 0.5s ease-in-out;
+  -webkit-transition: width 0.5s ease-in-out;
+  border-bottom: 2px solid #8eb8de;
 }
-.e-profile:hover:before{
+.e-profile:hover:before {
   width: 125%;
-  
 }
-.e-profile:hover{
-  color:#8eb8de;
+.e-profile:hover {
+  color: #8eb8de;
 }
 .videos {
   width: 100%;
@@ -253,7 +245,8 @@ export default {
   z-index: 10;
 }
 .doubleimge :nth-child(1) {
-  background: url(/Photo/A7M2_208.jpg) no-repeat;
+  background: url(http://arthur2.oss-cn-beijing.aliyuncs.com/Photopage/A7M2_208.jpg)
+    no-repeat;
   width: 50%;
   height: 80vh;
   background-position: center;
@@ -261,7 +254,8 @@ export default {
   background-attachment: fixed;
 }
 .doubleimge :nth-child(2) {
-  background: url(/Photo/_DSC3902.jpg) no-repeat;
+  background: url(http://arthur2.oss-cn-beijing.aliyuncs.com/Photopage/_DSC3902.jpg)
+    no-repeat;
   width: 50%;
   height: 80vh;
   background-position: center;
@@ -308,6 +302,4 @@ export default {
     opacity: 1;
   }
 }
-
-
 </style>
